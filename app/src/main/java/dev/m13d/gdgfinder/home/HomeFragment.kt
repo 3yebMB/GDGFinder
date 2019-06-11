e2f030f289a86cb413548c06dd54afb943efe275
Michael <3yebmb@gmail.com>
2019-06-11 11:03:46 +0500

    initail GDGFinder project

---
+++ app/src/main/java/dev/m13d/gdgfinder/home/HomeFragment.kt
@@ -0,0 +1,28 @@
+package dev.m13d.gdgfinder.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import dev.m13d.gdgfinder.R
+
+class HomeFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
+
+    private lateinit var viewModel: HomeViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.home_fragment, container, false)
+        viewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
+
+        return view
+    }
+}
